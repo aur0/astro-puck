530214771d941bcdd7601e01f36382f3e5da1514
James Warren <ted3@protonmail.com>
2025-07-03 08:49:33 +0000

    tailwind

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,31 +1,5 @@
 import { Render } from "@measured/puck";
-import { LazyMotion, domAnimation, m } from "motion/react"
-
-const config = {
-    components: {
-      HeadingBlock: {
-        fields: {
-          title: {
-            type: "text",
-          },
-        },
-        render: ({ title }) => {
-          return (
-            <LazyMotion features={domAnimation}>
-              <m.h1 
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className="font-size-5"
-              >
-                {title}
-              </m.h1>
-            </LazyMotion>
-          );
-        },
-      },
-    },
-  };
+import config from "../../puck.config.tsx";
 
 export default function Editor({ data }) {
   return <Render config={config} data={data} />;
